9389bc0bb9b46bd739a589af8f903e089f55b58e
jantzen16 <jantzen.bautista@gmail.com>
2024-10-27 20:49:51 +0800

    upload sql files

--- sql_load/queries.sql
+++ sql_load/queries.sql
@@ -228,15 +228,16 @@ SELECT COUNT(job_id) AS company_count,
     END as company_name
 FROM job_postings_fact
 WHERE company_id IN (572, 49, 1148)
-GROUP BY company_id with remote_job_skills AS (
-        SELECT skill_id,
-            COUNT(*) AS skill_count
-        FROM skills_job_dim AS skills_to_job
-            INNER JOIN job_postings_fact AS job_postings ON job_postings.job_id = skills_to_job.job_id
-        WHERE job_postings.job_work_from_home = True
-            AND job_postings.job_title_short = 'Data Analyst'
-        GROUP BY skill_id
-    )
+GROUP BY company_id;
+WITH remote_job_skills AS (
+    SELECT skill_id,
+        COUNT(*) AS skill_count
+    FROM skills_job_dim AS skills_to_job
+        INNER JOIN job_postings_fact AS job_postings ON job_postings.job_id = skills_to_job.job_id
+    WHERE job_postings.job_work_from_home = True
+        AND job_postings.job_title_short = 'Data Analyst'
+    GROUP BY skill_id
+)
 SELECT skills.skill_id,
     skills AS skill_name,
     skill_count
